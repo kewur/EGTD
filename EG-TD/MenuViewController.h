b8698c9f7f86a72b7279fcb1b34522fa6a63451b
Efe Burak <efe@randomg.com>
2011-10-08 00:32:44 +0300

    MenuViewController ekledim

---
+++ EG-TD/MenuViewController.h
@@ -0,0 +1,16 @@
+//
+//  MenuViewController.h
+//  EG-TD
+//
+//  Created by metin okur on 08.10.2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface MenuViewController : UIViewController {
+    
+}
+
+@end
